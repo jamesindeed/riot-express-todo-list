a65ac8b372f9b94617300a532024b20114e28c7a
James Troughton <jamesstroughton@gmail.com>
2020-09-12 13:35:04 +1200

    Updated .travis.yml and changed IP for deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '34.207.63.215',
+      host: '3.90.81.140',
       username: 'ubuntu',
       privateKey: 'new-key.pem'
     })
